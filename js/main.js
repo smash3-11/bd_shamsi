5e823ebfbaefa1e254a56003d6c8fa9bf68bdb9c
Sokhib <websoon010@gmail.com>
2023-08-16 23:21:00 +0500

    first commit

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
     hh = h / 2,
 
     opts = {
-        strings: ['С днём рождения!', 'Азиз','(≧▽≦)'],
+        strings: ['Happy birth day!','Shamsi','(≧▽≦)'],
         charSize: 30,
         charSpacing: 35,
         lineHeight: 40,
